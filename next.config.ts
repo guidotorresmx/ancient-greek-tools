64057949d61ed60aa0ba4e0439fb0c99164bb1fe
Marco Guido <guidotorresmx@gmail.com>
2026-03-16 01:00:46 -0700

    adding github site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/ancient-greek-tools" : "",
+  assetPrefix: isProd ? "/ancient-greek-tools/" : "",
+  trailingSlash: true,
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
